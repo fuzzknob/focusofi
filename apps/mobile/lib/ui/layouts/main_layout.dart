9583472089082ceb19e98b3324aefcc359f42cc3
Gagan Rai <gagan_rai@outlook.com>
2025-04-23 21:18:01 +0545

    feat: change mobile app name to Focusofi

--- apps/mobile/lib/ui/layouts/main_layout.dart
+++ apps/mobile/lib/ui/layouts/main_layout.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pomo_mobile/providers/background.dart';
 
-class MainLayout extends StatelessWidget {
+class MainLayout extends ConsumerStatefulWidget {
   const MainLayout({super.key, this.children});
 
   final List<Widget>? children;
 
   @override
+  ConsumerState<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends ConsumerState<MainLayout> {
+  @override
+  initState() {
+    super.initState();
+    _getBackground();
+  }
+
+  Future<void> _getBackground() async {
+    await ref.read(getBackgroundServiceProvider)();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final background = ref.watch(backgroundProvider);
+
     return Scaffold(
       body: Stack(
         alignment: Alignment.center,
         children: [
           Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                  'https://media.giphy.com/media/Basrh159dGwKY/giphy.gif',
-                ),
-              ),
+              color: Colors.purple.shade500,
+              image:
+                  background.isNotEmpty
+                      ? DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(background),
+                      )
+                      : null,
             ),
           ),
           Positioned.fill(
             child: Container(color: Colors.black.withValues(alpha: 0.5)),
           ),
-          ...?children,
+          ...?widget.children,
         ],
       ),
     );
